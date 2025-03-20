d557384a8a7f55ffdb3714ce501d6167142b42e8
Mike Terry <support@miketerry.org>
2025-03-20 00:10:50 -0500

    finished implementing and testing Check.isDate

--- test/check_isDate.test.js
+++ test/check_isDate.test.js
@@ -6,7 +6,7 @@
 const Check = require("../lib/utils/check");
 
 describe(`isDate`, () => {
-  describe(`Date object`, () => {
+  describe(`typeof = Date object`, () => {
     it(`should be a valid Date object`, () => {
       const dob = new Date("1945-01-23");
       let data = { dob };
@@ -18,6 +18,43 @@ describe(`isDate`, () => {
 
     it(`should be an invalid Date object`, () => {
       let data = { dob: new Date("1945-01-32") };
+      let checker = new Check(data);
+      checker.isDate("dob", undefined);
+      expect(checker.errors.length).toBe(1);
+    });
+  });
+
+  describe(`typeof = string`, () => {
+    it(`should be valid "mm-dd-yyyy" format`, () => {
+      let data = { dob: "01/23/1945" };
+      let checker = new Check(data);
+      checker.isDate("dob", undefined);
+      expect(checker.errors.length).toBe(0);
+    });
+
+    it(`should be invalid "mm-dd-yyyy" format`, () => {
+      let data = { dob: "01/32/1945" };
+      let checker = new Check(data);
+      checker.isDate("dob", undefined);
+      expect(checker.errors.length).toBe(1);
+    });
+
+    it(`should be valid "yyyy-mm-dd" format`, () => {
+      let data = { dob: "1945-01-23" };
+      let checker = new Check(data);
+      checker.isDate("dob", undefined);
+      expect(checker.errors.length).toBe(0);
+    });
+
+    it(`should be invalid "yyyy-mm-dd" format`, () => {
+      let data = { dob: "1945-01-32" };
+      let checker = new Check(data);
+      checker.isDate("dob", undefined);
+      expect(checker.errors.length).toBe(1);
+    });
+
+    it(`should be invalid "dd-mm-yyyy" format`, () => {
+      let data = { dob: "23/01/1945" };
       let checker = new Check(data);
       checker.isDate("dob", undefined);
       expect(checker.errors.length).toBe(1);
